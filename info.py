9ad3f42dc33ad2320bdfd90125748b1f60b39fb1
msdohni <141110310+msdohni@users.noreply.github.com>
2023-11-30 12:11:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://herokubin3:herokubin3@cluster0.8j1d0dh.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "herokubin3")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://1fc31e6568247d:9TEZ.kFwXN#P4Bs@cluster0.euvmy3e.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "1fc31e6568247d")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
