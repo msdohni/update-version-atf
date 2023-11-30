61ed9f4abfb1656c27d1b70217ba294e6f4ad8f6
msdohni <141110310+msdohni@users.noreply.github.com>
2023-11-30 19:00:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,10 +48,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "msleech03")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7b70b5e9ca066ed1a6c2b7879e0d83b649ab886e')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
